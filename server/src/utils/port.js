45fcf04e4ca67c5fe1fe24bbd70513f0c9a3d3bf
Yuktha Shree P <yukthashree456@gmail.com>
2026-06-12 10:37:52 +0530

    .

---
+++ server/src/utils/port.js
@@ -0,0 +1,17 @@
+import net from "node:net";
+
+export function isPortAvailable(port) {
+  return new Promise((resolve) => {
+    const server = net.createServer();
+
+    server.once("error", () => {
+      resolve(false);
+    });
+
+    server.once("listening", () => {
+      server.close(() => resolve(true));
+    });
+
+    server.listen(port);
+  });
+}
